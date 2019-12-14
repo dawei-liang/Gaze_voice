055800dc33c76b295ee6b856ef984fc28635d184
David <32776482+dawei-liang@users.noreply.github.com>
2019-12-14 16:07:06 -0600

    Add files via upload

--- convert_wav.py
+++ convert_wav.py
@@ -13,6 +13,8 @@ import numpy as np
 from scipy.io import wavfile
 import resampy
 from scipy import signal
+import pandas as pd
+import os
 
 #%%
 
@@ -87,6 +89,44 @@ def framing(data, window_length, hop_length):
             frame_array = np.concatenate((frame_array, frame_temp), axis=0)
         start += hop_length
     return frame_array
+
+
+def reconstruct_time_series(frames, hop_length_samples):
+    """
+    Reconstruct N-Dimensional framed array back to (N-1)-Dimensional frames or 1D time series signals
+    Args:
+        frames = [# of frames, window length1 in samples, (window length2, ...)]
+        hop_length_samples = # of samples skipped between two frames
+    return:
+        (N-1)-Dimensional frames or 1D time series signals
+    """
+    new_signal = []
+    for i in range(len(frames)-1):
+        for j in range(0, hop_length_samples):
+            new_signal.append(frames[i, j])
+    # Last frame
+    for i in range(frames.shape[1]):
+        new_signal.append(frames[-1,i])
+        
+    new_signal = np.asarray(new_signal)
+    
+    return new_signal
+
+
+def noise_filter(audio_data, order=10, cutoff=[50, 6000], sr=16000):
+    """
+    Butterworth filter
+    Args: 
+        audio_data: raw audio signals
+        order: filter order
+        cutoff: cutoff freq (1/sqrt(2)), can be float or array of two floats
+        sr: sampling rate
+    Out:
+        filtered audio signals
+    """
+    sos = signal.butter(order, cutoff, 'bp', fs=sr, output='sos')
+    filtered_audio = signal.sosfilt(sos, audio_data)
+    return filtered_audio
         
     
 def plot_spec(audio_data, sr, window_length, hop_length):
@@ -117,27 +157,62 @@ def plot_spec(audio_data, sr, window_length, hop_length):
     plt.ylabel('|Y(freq)|')
     plt.show()
             
+
+
 #%%
-    
-path_load = './original_data/71_merge.mp4'
-path_save = './new_data/71_merge.wav'
-transform(path_load, path_save, on = False)
-
-sr, audio_data = read_audio_data(path_save)
-sr_new = 16000
-audio_data = mono(audio_data, sr, sr_new)
-FourierTransformation = np.abs(sp.fft(audio_data))
-
 order = 10
-cutoff = [50,6000]
-# Butterworth filter
-sos = signal.butter(order, cutoff, 'bp', fs=sr_new, output='sos')
-filtered_audio = signal.sosfilt(sos, audio_data)
+cutoff = [1000,6000]
+window = (1/75)   # seconds
+hopping = window * 1   # seconds
+sr_new = 16050
 
+#noise_filter(audio_data, order, cutoff, sr_new)
 #plot_spec(audio_data, sr_new, int(0.05*sr_new), int(0.05*sr_new))
 
+
+ref = pd.read_csv('./original_data/360_speech_audio_video/label_gaze_table.csv')
+id_list = set(ref.id.values)
+path_load = './original_data/360_speech_audio_video/'
+video_file_list = [x for x in os.listdir(path_load) if x.endswith('.mp4')]   # Load video files
+
 #%%
-path_write = './new_data/71_merge_filtered.wav'
-write_audio_data(path_write, sr_new, filtered_audio)
+# Loop for each subject
+for i in id_list:
+    for video in video_file_list:
+        if str(i) in video:
+            target = video
+    # transform from mp4 to wav
+    path_save = './transformed_data/' + target.replace('.mp4', '') + '.wav'
+    print('transforming video %d to wav' %i)
+    transform(path_load + target, path_save, on = True)  
+    
+    # Load wav for segmentation
+    print('begin to segment wave file %d' %i)
+    sr, audio_data = read_audio_data(path_save)
+    audio_data = mono(audio_data, sr, sr_new)
+    framed_audio = framing(audio_data, int(window * sr_new), int(hopping * sr_new))
+    
+    # segmentation
+    frames_background, frames_person = np.empty((1, framed_audio.shape[1])), np.empty((1, framed_audio.shape[1]))
+    start_background = (ref.loc[(ref['id'] == i) & (ref['roi1_person_yn'] == 'Background'), 'fix_start_frame']) * ((1/75) // hopping) - 1
+    end_background = (ref.loc[(ref['id'] == i) & (ref['roi1_person_yn'] == 'Background'), 'fix_end_frame']) * ((1/75) // hopping) - 1
+    start_person = (ref.loc[(ref['id'] == i) & (ref['roi1_person_yn'] == 'person'), 'fix_start_frame']) * ((1/75) // hopping) - 1
+    end_person = (ref.loc[(ref['id'] == i) & (ref['roi1_person_yn'] == 'person'), 'fix_end_frame']) * ((1/75) // hopping) - 1   
+    frames_background_idx = np.vstack((start_background.values, end_background.values)).T
+    frames_person_idx = np.vstack((start_person.values, end_person.values)).T
+    
+    # stack sound segments based on categories
+    for j in range(len(frames_background_idx)):  
+        frames_background = np.vstack((frames_background, framed_audio[int(frames_background_idx[j,0]) : int(frames_background_idx[j,1])]))
+    for k in range(len(frames_person_idx)):        
+        frames_person = np.vstack((frames_person, framed_audio[int(frames_person_idx[k,0]) : int(frames_person_idx[k,1])]))
+    
+    # reconstructed audio signals for each category per subject, idx from 1:end
+    audio_background = reconstruct_time_series(frames_background[1:], int(hopping * sr_new))
+    audio_person = reconstruct_time_series(frames_person[1:], int(hopping * sr_new))
+    # save segmented wav for each subject and each categories seperately
+    write_audio_data('./segmented_data/background/' + target.replace('_merge.mp4', '') + '.wav', sr_new, audio_background)
+    write_audio_data('./segmented_data/person/' + target.replace('_merge.mp4', '') + '.wav', sr_new, audio_person)
+
 
 
